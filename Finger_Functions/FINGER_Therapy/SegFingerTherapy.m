4ab371cf77273dd758e0b2ec0c6ec82982802892
Sumner15 <Sumner15@localhost>
2014-04-30 12:50:14 -0700

    Added segmentation for speed test data (in addition to sunshine day). Function SegFingerTherapy now outputs two matlab structures (segdata1 and segdata2) and saves those structures to .mat files in the subject's folder.

--- Finger_Functions/FINGER_Therapy/SegFingerTherapy.m
+++ Finger_Functions/FINGER_Therapy/SegFingerTherapy.m
@@ -1,4 +1,4 @@
-function segdata = SegFingerTherapy(username,subname)
+function [segdata1,segdata2] = SegFingerTherapy(username,subname)
 %SegFingerTherapy
 %
 % Segments FINGER therapy data into a sample by channel by trial array
@@ -7,7 +7,7 @@ function segdata = SegFingerTherapy(username,subname)
 %        username ast string, e.g. 'Sumner'
 
 
-% Load in subject .mat data file 
+%% Load in subject .mat data file, timing data, and head model
 
 switch username
     case 'Sumner'
@@ -28,7 +28,7 @@ else
     cd(strcat(subname,'/Exam 1/'))    
 end
 
-
+% Read in .mat file
 filename = celldir([subname '*.mat']);
 filename = filename{1}(1:end-4);
 disp(['Loading ' filename '...']);
@@ -38,10 +38,12 @@ load(filename);
 cd .. ; cd ..;
 load('note_timing_SunshineDay') %creates var speedTest   <70x1 double>
 load('note_timing_SpeedTest')   %creates var sunshineDay <43x1 double>
+% note: speedTest only includes notes spaced > 1500ms
 
 % load the EGI head model
 load egihc256redhm
-segdata.hm = EGIHC256RED;
+segdata1.hm = EGIHC256RED;
+segdata2.hm = EGIHC256RED;
 
 %% info regarding the experimental setup
 sr = samplingRate;
@@ -53,39 +55,46 @@ numtrials1 = length(sunshineDay);   % Number of notes in Sunshine Day
 numtrials2 = length(speedTest);     % Number of notes in Speed Test
 
 %% Initialize data structure components
-segdata.sr = sr;
-segdata.data = zeros(sr*triallength,nchans,numtrials1);
+segdata1.sr = sr; segdata2.sr = sr;
+segdata1.data = zeros(sr*triallength,nchans,numtrials1);
+segdata2.data = zeros(sr*triallength,nchans,numtrials2);
 
-%% Create marker spike train
+%% Create marker spike trains
 
 % Marker data for FINGER Game
 filename = strrep(filename,' ','_');
 marker1 = eval([filename '2Video_trigger']);
-%marker1 = JOHG_20140122_12232Video_trigger;
+marker2 = eval([filename '3Video_trigger']);
 ind1 = find(getspike(marker1) > 0)-2000;
+ind2 = find(getspike(marker2) > 0)-2000;
 
 for n = 1:numtrials1
-    segdata.marker1(1,ind1+sunshineDay(n)) = 1;    
+    segdata1.marker1(1,ind1+sunshineDay(n)) = 1;    
 end
 
-marker1inds = find(segdata.marker1 > 0);
+for n = 1:numtrials2
+    segdata2.marker2(1,ind2+speedTest(n)) = 1;    
+end
 
-% Marker data for Speed Test
-% parse in info from text file into marker2
-% save this variable as segdata.markers(2,:)
-%marker2 = JOHG_20140122_12233Video_trigger;
-%ind2 = find(getspike(marker2) > 0);
+marker1inds = find(segdata1.marker1 > 0);
+marker2inds = find(segdata2.marker2 > 0);
 
 
 %% Load, filter, and segment EEG data
 
-eeg = eval([filename '2']);
+eeg1 = eval([filename '2']);
+eeg2 = eval([filename '3']);
 disp('Filtering the data...');
-eeg = filtereeg(eeg(EGIHC256RED.ChansUsed,:)',sr);
+eeg1 = filtereeg(eeg1(EGIHC256RED.ChansUsed,:)',sr);
+eeg2 = filtereeg(eeg2(EGIHC256RED.ChansUsed,:)',sr);
   
 % segment into sample x channel x trial matrix
 for t = 1:numtrials1
-    segdata.data(:,:,t) = eeg(marker1inds(t)-(sr*1.5):marker1inds(t)+(sr*1.5)-1,:);
+    segdata1.data(:,:,t) = eeg1(marker1inds(t)-(sr*1.5):marker1inds(t)+(sr*1.5)-1,:);
+end
+
+for t = 1:numtrials2
+    segdata2.data(:,:,t) = eeg2(marker2inds(t)-(sr*1.5):marker2inds(t)+(sr*1.5)-1,:);
 end
 
 disp('Saving segmented data...');
@@ -94,6 +103,7 @@ if ispc == 1
 else
     cd(strcat(subname,'/Exam 1/'))
 end
-save(strcat(subname,'_segdata'),'segdata');
+save(strcat(subname,'_segdata1'),'segdata1');
+save(strcat(subname,'_segdata2'),'segdata2');
 
 end
