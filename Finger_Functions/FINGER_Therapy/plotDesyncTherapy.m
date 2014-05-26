6865ffa3fe3bfe010568908bd83ad2d8af8f75fd
Sumner15 <28747498+sumner15@users.noreply.github.com>
2014-05-26 12:16:54 -0700

    Rewrote plot desync therapy. It now takes in the spectra cell array of structtures and freq as a frequency in Hz. It results in plots of the spectral results (magnitude normalized % power)of the given frequency across time for all four conditions. Irrespective of input freq, plotDesyncTherapy also forms and imagesc 2D heat map array image of all frequncy bins x time for all four conditions.

--- Finger_Functions/FINGER_Therapy/plotDesyncTherapy.m
+++ Finger_Functions/FINGER_Therapy/plotDesyncTherapy.m
@@ -1,158 +1,114 @@
-function PlotMuDesync(spectra,freq)
+function plotDesyncTherapy(spectra,freq)
 %PlotMuDesync(spectra, freq)
 % Plots mu over time by using the spectra variable as it is loaded from the
 % subjects finalcean.mat data file.
 %
 % spectra: cell array of structures containing data from finalclean.mat
-% freq: desired frequency bin
+% freq: desired frequency lower bound in Hz. (e.g. freq = 8 will give the
+% 8-12 Hz band).
 
 if ~exist('spectra','var')
     error('please load finalclean, and verify existence of var: spectra');
 end  
+clf; close all;
 
-%% Raw Power
-figure;
+%% checking that the frequency given was valid.
+freqInd = find(spectra{1}.freqs==freq);
+if isempty(freqInd)
+    error(['please select a valid frequency: ' num2str(spectra{1}.freqs)]);
+end
 
-freqstr = num2str(spectra{1}.freqs(spectra{1}.freqs == freq));
-freq = find(spectra{1}.freqs == freq);
-
-time = -1500:125:1375;
+% creating string for plotting title purposes
+freqrez = spectra{1}.freqs(2)-spectra{1}.freqs(1);
+freqstr = [num2str(freq-freqrez/2) '-' num2str(freq+freqrez/2)];
 
 %% Channel selection TEMPORARY SOLUTION ONLY!!!
-%LM_128 = [7,31,37,42,32,38,43,48];
-%RM_128 = [];
-LM_256 = [];        
-RM_256 = [];
+LM_256 = [71  72  75  76  77  78  79  80  63  64  65  66  57  58  59 ...
+          60  50  51  52  53  43  44  45  87  88  89];    
+RM_256 = [132 185 197 144 184 196 205 204 195 183 155 164 182 194 203 ...
+          173 181 131 143 154 163 172 180 130 142 153 ];      
 
-hand = input('Which hand was tested (impaired hand)? (type L or R)','s');
-if     (hand == 'L'),  elecs = RM_256;
-elseif (hand == 'R'),  elecs = LM_256;
+hand = input('Which hand was tested (impaired hand)? (type L or R): ','s');
+if     (hand == 'L'),  elecs = RM_256; %cont. cort LH = RM
+elseif (hand == 'R'),  elecs = LM_256; %cont. cort RH = LM
 else   error('improper selection of motor cortex channels');
 end
 
+newChans = zeros(1,length(elecs));
+for i = 1:length(elecs)
+    tempInd = find(spectra{1}.hm.ChansUsed==elecs(i));
+    newChans(i) = tempInd;   
+end
+elecs = newChans;
 
-%% Plotting results
-hold on;
-plot(time,squeeze(mean(spectra{1}.eegpower_chunks(freq,elecs,:),2)),'r')
-plot(time,squeeze(mean(spectra{2}.eegpower_chunks(freq,elecs,:),2)),'b')
-plot(time,squeeze(mean(spectra{3}.eegpower_chunks(freq,elecs,:),2)),'g')
+%% Computing power, normalized power, and plotting results
+%creating time vector for plotting
+time = (-1500:250:1375)';
 
-xlabel('Time (sec rel to movement)');
-ylabel('Power (uV^2)');
-title(strcat('Left Motor Cortex : ',freqstr,'Hz Power Over Time'));
+figure; hold on;
+colors = ['r' 'g' 'b' 'k']; 
 
-legend('Active','Passive','No Stim','Location','Best');
+for i = 1:4
+    % computing power across all chosen electrodes at the chosen frequency
+    power{i} = squeeze(mean(spectra{i}.eegpower_trials(freqInd,elecs,:),2));
+    % computing normalized power to first window of trial 
+    normPower{i} = power{i}/power{i}(1)-1;
+    % plotting result vs. time
+    plot(time,normPower{i},colors(i));
+end
 
-%ylim([0.5*10^6 1.5*10^6]);
+xlabel('Time (msec rel to not reaching target)');
+ylabel('Normalized Power (% change)');
+title(strcat('Contralateral Motor Cortex : ',freqstr,'Hz Power'));
 
-%% Normalized Power
+legend(spectra{1}.cond,spectra{2}.cond,spectra{3}.cond,spectra{4}.cond,'Location','Best');
+axis([time(1) time(end) -1 1]);
 
-figure;
-
-restingpower1 = ones(length(time),1)*squeeze(mean(spectra{1}.eegpower_chunks(freq,elecs,1),2));
-restingpower2 = ones(length(time),1)*squeeze(mean(spectra{2}.eegpower_chunks(freq,elecs,1),2));
-restingpower3 = ones(length(time),1)*squeeze(mean(spectra{3}.eegpower_chunks(freq,elecs,1),2));
-
-trialpower1 = squeeze(mean(spectra{1}.eegpower_chunks(freq,elecs,:),2));
-trialpower2 = squeeze(mean(spectra{2}.eegpower_chunks(freq,elecs,:),2));
-trialpower3 = squeeze(mean(spectra{3}.eegpower_chunks(freq,elecs,:),2));
-
-normpower1 = trialpower1./restingpower1;
-normpower2 = trialpower2./restingpower2;
-normpower3 = trialpower3./restingpower3;
-
-normpower1 = normpower1';
-normpower2 = normpower2';
-normpower3 = normpower3';
-
-hold on;
-plotx(time,log10(normpower1),'r');
-plotx(time,log10(normpower2),'b');
-plotx(time,log10(normpower3),'g');
-
-xlabel('Time (sec rel to movement)');
-ylabel('Power (uV^2)');
-title(strcat('Contralateral Motor Cortex : ',freqstr,'Hz Normalized Power Over Time'));
-
-legend('Active','Passive','No Stim','Location','Best');
+%% Computing normalized power for all frequency bins
+clear power normPower
+power = cell(1,4); normPower = cell(1,4);
+nFreqBins = size(spectra{1}.eegpower_trials,1);
+nWindows  = size(spectra{1}.eegpower_trials,3);
+for song = 1:4
+    power{song} = zeros(nFreqBins,nWindows);
+    for freqBin = 1:size(spectra{song}.eegpower_trials,1)   
+       % computing power across all chosen electrodes at the current freq
+       power{song}(freqBin,:) = squeeze(mean(spectra{song}.eegpower_trials(freqBin,elecs,:),2))';
+       % computing normalized power to first window of trial
+       normPower{song}(freqBin,:) = power{song}(freqBin,:)/power{song}(freqBin,1)-1;
+    end   
+end
 
 %% Imagesc of frequency x time
 
 figure;
-
-colormap jet
-
-time = -1.5:.125:1.375;
-
-restingpower1 =  squeeze(mean(spectra{1}.eegpower_chunks(:,elecs,1),2))*ones(1,size(spectra{1}.eegpower_chunks,3));
-restingpower2 =  squeeze(mean(spectra{2}.eegpower_chunks(:,elecs,1),2))*ones(1,size(spectra{1}.eegpower_chunks,3));
-restingpower3 =  squeeze(mean(spectra{3}.eegpower_chunks(:,elecs,1),2))*ones(1,size(spectra{1}.eegpower_chunks,3));
-
-trialpower1 = squeeze(mean(spectra{1}.eegpower_chunks(:,elecs,:),2));
-trialpower2 = squeeze(mean(spectra{2}.eegpower_chunks(:,elecs,:),2));
-trialpower3 = squeeze(mean(spectra{3}.eegpower_chunks(:,elecs,:),2));
-
-normpower1 = trialpower1./restingpower1;
-normpower2 = trialpower2./restingpower2;
-normpower3 = trialpower3./restingpower3;
-
-normpower1 = normpower1';
-normpower2 = normpower2';
-normpower3 = normpower3';
-
-maxpower(1) = max(max(normpower1));
-maxpower(2) = max(max(normpower2));
-maxpower(3) = max(max(normpower3));
-
-minpower(1) = min(min(normpower1));
-minpower(2) = min(min(normpower2));
-minpower(3) = min(max(normpower3));
-
-maxp = log10(max(maxpower));
-minp = log10(min(minpower));
-
-
-for k = 1:length(spectra{1}.freqs)
-     strfreqs{k} = num2str(spectra{1}.freqs(k));
-end
-for k = 1:length(time)
-     strtimes{k} = num2str(time(k));
-end
-
-subplot(3,1,1);
-h(1) = imagesc(log10(normpower1)',[minp maxp]);
-colorbar
-set(gca,'YTick',1:1:length(spectra{1}.freqs))
-set(gca,'YTickLabel',strfreqs)
-set(gca,'XTick',1:1:length(time))
-set(gca,'XTickLabel',strtimes)
-ylabel('Frequency (Hz)');
-xlabel('Time (msec rel movement)');
-title('Active Condition');
-
-subplot(3,1,2);
-h(2) = imagesc(log10(normpower2)',[minp maxp]);
-colorbar
-set(gca,'YTick',1:1:length(spectra{1}.freqs))
-set(gca,'YTickLabel',strfreqs)
-set(gca,'XTick',1:1:length(time))
-set(gca,'XTickLabel',strtimes)
-ylabel('Frequency (Hz)');
-xlabel('Time (msec rel movement)');
-title('Passive Condition');
-
-subplot(3,1,3);
-h(3) = imagesc(log10(normpower3)',[minp maxp]);
-colorbar
-set(gca,'YTick',1:1:length(spectra{1}.freqs))
-set(gca,'YTickLabel',strfreqs)
-set(gca,'XTick',1:1:length(time))
-set(gca,'XTickLabel',strtimes)
-ylabel('Frequency (Hz)');
-xlabel('Time (msec rel movement)');
-title('No Stim Condition');
+for i = 1:4
+    subplot(2,2,i)
+%     %determining min and max power for plotting
+%     minp = min(min(normPower{i})); maxp = max(max(normPower{i}));
     
-
-
+    % plotting results
+    h(1) = imagesc(normPower{i}(:,2:end),[-0.5 0.5]);
+    colorbar
+    
+    %flipping y axis back to normal and labeling (frequency)
+    set(gca,'YDir','normal') 
+    set(gca,'YTick',1:1:length(spectra{1}.freqs))    
+    for ii = 1:length(spectra{i}.freqs)
+        strfreqs{ii} = num2str(spectra{i}.freqs(ii)); end
+    set(gca,'YTickLabel',strfreqs)
+        
+    %labeling x axis ticks (time)
+    set(gca,'XTick',1:1:length(time)-1)
+    for ii = 1:length(time)-1
+        strtimes{ii} = num2str(250*ii-1500); end
+    set(gca,'XTickLabel',strtimes)
+    
+    % labels and titles
+    ylabel('Frequency (Hz)');
+    xlabel('Time (msec rel movement)');
+    title(spectra{i}.cond);
+end
+    
 end
 
