0acc7740e6e9ad22c6b9173cb04fbf2c1145936c
Sumner15 <Sumner15@localhost>
2014-10-31 14:53:27 -0700

    Minor fixes/changes to topographical plotting - plots to subplots rather than time course - fixed a bad freq bucket index reference (was going to actual freq not ind)

--- Finger_Functions/FINGER_Enviro/topography.m
+++ Finger_Functions/FINGER_Enviro/topography.m
@@ -1,8 +1,9 @@
 function topography(username,subname)
 % options
-N = 100;        % Sample length 
-fs = 1000;      % sampling frequency (Hz)
-fVec = linspace(0,fs/2,N/2+1); % frequency vector resolved by fft
+N = 200;                        % Sample length 
+fs = 1000;                      % sampling frequency (Hz)
+fVec = linspace(0,fs/2,N/2+1);  % frequency vector resolved by fft
+nWins = floor(fs*3/N);          % number of windows per epoch (3 sec)
 
 freq = 11;      % Hz (desired center freq used for topography)
 [c freqInd] = min(abs(fVec-freq)); % freq converted to an index
@@ -20,12 +21,12 @@ load egihc256hm; hm = EGIHC256;
 %% computing FFT
 % data = samples x channels
 nSamples = size(data,2); nChans = size(data,1); % common vars
-fdata = NaN(nChans,floor(nSamples/100));         %preparing data structure
+fdata = NaN(nChans,floor(nSamples/N));          %preparing data structure
 
-for window = 1:floor(nSamples/100)   % moving along in 100ms windows   
-   sampleWin = (window-1)*100+1:window*100;     % preparing sample window
+for window = 1:floor(nSamples/N)   % moving along in 100ms windows   
+   sampleWin = (window-1)*N+1:window*N;         % preparing sample window
    feeg = fft(data(:,sampleWin)')';             % taking fft of data
-   feeg = squeeze(mean(abs(feeg(:,freq)),2));   % computing power in f band
+   feeg = squeeze(mean(abs(feeg(:,freqInd)),2));% computing power in f band
    fdata(:,window) = feeg';                     % filling in data structure
    % fdata = channels x 100msWindowNumber 
 end
@@ -40,11 +41,11 @@ cd(subname);
  %start index of time sample marking beginning of trial (from labjack)
  startInd = min(find(abs(concatData.vid{condition})>2000));
  %markerInds is an integer vector of marker indices (rounded to 100ms)
- markerInds = round((startInd+round(blackBird))/100);
+ markerInds = round((startInd+round(blackBird))/N);
 
  for trial = 1:length(markerInds)
-    if trial==1; sumTrials=zeros(nChans,31); end;
-    trialData = fdata(:,markerInds(trial)-15:markerInds(trial)+15);
+    if trial==1; sumTrials=zeros(nChans,nWins); end;    
+    trialData = fdata(:,markerInds(trial)-floor(nWins/2):markerInds(trial)+floor(nWins/2));
     sumTrials = sumTrials+trialData;
  end
  % trialPower = channels x 100msTrialWindowNumber(31 total)
@@ -52,13 +53,14 @@ cd(subname);
  
  
 %% plotting data
-scrsz = get(0,'ScreenSize'); 
+scrsz = get(0,'ScreenSize'); scrsz = [1 1 1000 800];
 set(figure,'Position',scrsz)
-for i = 1:31
-    corttopo(trialPower(:,i),hm);    
-    title(['trialTime = ' num2str(i/10) ' sec']);
-    %set(gca,'clim',[0 5]) 
+for i = 1:nWins
+    subplot(3,5,i);
+    corttopo(trialPower(:,i),hm,'drawelectrodes');    
+    title(['trialTime = ' num2str(i*N/fs) ' sec']);
+    set(gca,'clim',[-200 200]) 
     
     if i==1; pause(1); end;
-    pause(0.001);
+    pause(0.010);
 end
