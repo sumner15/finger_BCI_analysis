0e61c65e0838ae58988e43a8ff27da558db8072b
Sumner15 <28747498+sumner15@users.noreply.github.com>
2015-02-23 08:38:51 -0800

    Finger Therapy :: Processed 8 more subjects. Initial work on artifact rejection.

--- Finger_Functions/FINGER_Therapy/processTherapyData.m
+++ Finger_Functions/FINGER_Therapy/processTherapyData.m
@@ -1,33 +1,38 @@
 clc; clear concatData; 
 
-subjects = {{'AGUJ'}};
-%       ,{'BROR'},{'CORJ'},{'CROD'},{'ESCH'},{'FLOA'},...
+% ------ COMPLETED (01/28/2015 ---------------------------------------- %
+% subjects =   {{'AGUJ'},{'BROR'},{'CORJ'},{'CROD'},{'ESCH'},{'FLOA'},...
 %               {'GONA'},{'HAAN'},{'JOHG'},{'LEUW'},{'NGUT'},{'RITJ'},...
-%               {'SARS'},{'WHIL'},{'WILJ'},{'WRIJ'}};         
-  
-% SUBJECTS NOT YET WORKING (11/20/2014) ------------------------------- %         
-%{'ARRS'},{'CHIB'},{'KILB'},{'LAMK'},{'LOUW'},{'MCCL'},{'MILS'},
-%{'VANT'},{'YAMK'} 
+%               {'SARS'},{'WHIL'},{'WILJ'},{'WRIJ'}};     
 %
-% missing post (not yet done)
-% {'ARRS'},{'CHIB'},{'MILS'}
-% missing post (shouldn't be missing)
-% {'KILB'},{'LAMK'}
-% raw data split up (can be fixed)
-% {'VANT'},{'YAMK'}
+% ------ COMPLETED (02/23/2015 ---------------------------------------- %
+% subjects =   {{'ARRS'},{'CHIB'},{'KILB'},{'LAMK'},...
+%               {'POOJ'},{'PRIJ'},{'VANT'},{'YAMK'}};
+% --------------------------------------------------------------------- %
+%
+% SUBJECTS NOT YET WORKING (02/23/2014) ------------------------------- %         
+%{'LOUW'},{'MALJ'},...
+%{'MCCL'},{'MILS'},
+%
 % problem with segmentation (index exceeds matrix
 % {'LOUW'}
 % problem with channelSelect/filtfilt
 % {'MCCL'}
+% EXCLUDED
+% {'MILS'} (corn-rows)
 % --------------------------------------------------------------------- %
 
 if (~exist('username','var'))
    username = input('Username: ','s'); 
 end
 
-tic
-% concatenate data (only needs to be ran once)
 concatBool = input('Would you like to Concatenate? Type y or n: ','s');
+processBool = input('Would you like to Process? Type y or n: ','s');
+computeBool = input('Would you like to Compute results? Type y or n: ','s');
+
+
+%% concatenate data 
+tic
 if concatBool == 'y'
     for currentSub = 1:length(subjects)       
         subname = subjects{currentSub};   
@@ -41,23 +46,37 @@ if concatBool == 'y'
         concatTherapy(username,subname);    %note: saves to file
     end
 end
-% process all data
-for currentSub = 1:length(subjects)
-    subname = subjects{currentSub};   
-    subname = subname{1};
-    
-    disp('-------------------------------------');
-    disp(['Beginning data processing for ' subname]);
-    disp('-------------------------------------');
-    
-    concatData = preProcessTherapy(username,subname);   clear ans
-    waveletData = waveletTherapy(concatData);   
-    waveletData = SegFingerTherapy(username,subname,waveletData);
-    %intraSubjectTherapy(username,subname,waveletData);
-    %plotSub(username,subname);        
+
+%% process all data
+if processBool == 'y'
+    for currentSub = 1:length(subjects)
+        subname = subjects{currentSub};   
+        subname = subname{1};
+
+        disp('-------------------------------------');
+        disp(['Beginning data processing for ' subname]);
+        disp('-------------------------------------');
+
+        concatData = preProcessTherapy(username,subname);   clear ans
+        waveletData = waveletTherapy(concatData);   
+        waveletData = SegFingerTherapy(username,subname,waveletData);            
+    end
 end
-%plotInterSub
+
+%% compute results
+if computeBool == 'y'
+    clear concatData waveletData 
+    disp('-------------------------------');
+    disp(' Beginning results computation ');
+    disp('-------------------------------');
+   for currentSub = 1:length(subjects)
+        subname = subjects{currentSub};   
+        subname = subname{1};                       
+        intraSubjectTherapy(username,subname);
+   end
+end
+
+%% finish up
 toc
 sendEmail;
-
 clear ans subname concatData selectData waveletData;
