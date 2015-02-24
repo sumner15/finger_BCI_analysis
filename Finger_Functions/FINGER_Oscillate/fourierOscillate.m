2077a8a44e8c860593623a10a2121b1a83ebe08e
Sumner15 <Sumner15@localhost>
2015-02-24 12:20:45 -0800

    Oscillate: Major revisions and additions.

--- Finger_Functions/FINGER_Oscillate/fourierOscillate.m
+++ Finger_Functions/FINGER_Oscillate/fourierOscillate.m
@@ -6,23 +6,41 @@ sr = subData.sr;                        % sampling rate
 nChans = size(subData.segEEG{1},2);     % number of active channels
 
 %% Find Fourier coeff & power across frequency bands for each trial
-subData.power = cell(size(subData.segEEG));
 for exam = 1:nExams
-   % initializing power array {exam}(trial,chan,fcoeffs)
-   subData.power{exam} = zeros(subData.nTrials,nChans,sr/2);
+   % initializing power arrays: {exam}(trial,chan,fcoeffs,epochs)
+   subData.power{exam}      = zeros(subData.nTrials,nChans,sr/2,subData.trialLength);
+   subData.breakPower{exam} = zeros(subData.nTrials,nChans,sr/2,subData.breakLength);
+   
    for trial = 1:subData.nTrials 
        for chan = 1:nChans
+           
            % fourier coefficient vector for exam/trial/channel
-           fCoeffs = zeros(256,1); 
+           % fCoeffs (frequencies, epochs in a trial)
+           fCoeffs = zeros(sr,subData.trialLength); 
            for second = 1:subData.trialLength
                 % fourier coefficient vector summed each second 
-                timeSpan = ((second-1)*subData.sr+1):(second*subData.sr);
-                fCoeffs = fCoeffs+fft(squeeze(subData.segEEG{exam}(trial,chan,timeSpan)));
-           end
-           % computing power, and cutting off negative freq components
-           fCoeffs = fCoeffs./subData.trialLength;
-           subData.power{exam}(trial,chan,:) = abs(fCoeffs(1:(end/2)));
-       end
-   end
-end
+                timeSpan = ((second-1)*subData.sr+1):(second*subData.sr); %1-sec in trial
+                fCoeffs(:,second) = fft(squeeze(subData.segEEG{exam}(trial,chan,timeSpan)));
+           end           
+           
+           % computing fcoeff vector for breaks (resting data)
+           % fCoeffs (frequencies, epochs in a trial)
+           fCoeffsBreak = zeros(sr,subData.breakLength);
+           for second = 1:subData.breakLength
+               % fourier coefficient vector summed each second
+               timeSpan = ((second-1)*subData.sr+1):(second*subData.sr); %1-sec in trial
+               fCoeffsBreak(:,second) = fft(squeeze(subData.segEEGBreak{exam}(trial,chan,timeSpan)));
+           end           
+           
+           % cutting off negative freq components, computing power, and
+           % filling power and break power arrays with data
+           fCoeffs      =      fCoeffs(1:sr/2,:);
+           fCoeffsBreak = fCoeffsBreak(1:sr/2,:);      
+           for freq = 1:sr/2
+            subData.power{exam}(trial,chan,freq,:)      = abs(fCoeffs(freq,:));
+            subData.breakPower{exam}(trial,chan,freq,:) = abs(fCoeffsBreak(freq,:));
+           end % freq
+       end % channel
+   end % trial
+end % exam
 
