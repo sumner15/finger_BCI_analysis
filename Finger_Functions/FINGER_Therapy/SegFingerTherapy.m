6c993000900920cd970ff306ffe4d92c9332f9ae
Sumner15 <28747498+sumner15@users.noreply.github.com>
2015-06-23 09:42:52 -0700

    Therapy: MAJOR update. This will be the revision used for the (hopefully) final cleaning (screen+ICA) of the data. Cleaning has been moved to before t-f decomposition. Trials are zero padded by a standard 1 s interval. Much of the data processing pipeline has been automated with several catches for common errors. The interface is now fully command based, other than the preprocessing parameters which are set manually in the code (once).

--- Finger_Functions/FINGER_Therapy/SegFingerTherapy.m
+++ Finger_Functions/FINGER_Therapy/SegFingerTherapy.m
@@ -1,4 +1,4 @@
-function waveletData = SegFingerTherapy(username,subname,waveletData)
+function waveletData = SegFingerTherapy(username,subname,saveBool,waveletData)
 %SegFingerEnviro
 %
 % Segments FINGER therapy study data into a 
@@ -8,17 +8,25 @@ function waveletData = SegFingerTherapy(username,subname,waveletData)
 %        username as string, e.g. 'Sumner'
 %        waveletData (optional) data set will speed up loading procedure
 
+%% loading data if necessary
+if exist('waveletData','var')
+    disp('Wavelet data passed directly; skipping load...');
+else
+    filename = celldir([subname '*waveletData.mat']);
+    filename{1} = filename{1}(1:end-4);
 
-%% Read in note/trial timing data 
-cd .. ; cd .. ;
+    fprintf(['Loading ' filename{1} '...']);
+    load(filename{1});  
+    fprintf('Done.\n');
+end
 %load('songName') %creates var songName <nNotes x 1 double> (ms)
+setPathTherapy(username);
 load('note_timing_SpeedTest')
 load('note_timing_SunshineDay')
-setPathTherapy(username,subname)
 
-%% info regarding the experimental setup
+%% information vars about data
 nSongs = length(waveletData.motorEEG);          % # songs per recording (6)
-triallength = 3;                                % length - one note trial (sec)
+trialLength = 3;                                % length - one note trial (sec)
 nTrials = [length(sunshineDay) length(speedTest)];  % Number of notes 
 nTrials = repmat(nTrials,[1 2]);                % (extending to 4 songs)
 sr = waveletData.sr;                            % sampling rate
@@ -26,66 +34,39 @@ nChans = size(waveletData.eeg{1},1);            % number of active channels
 nMotorChans = size(waveletData.motorEEG{1},1);  % number of ,motor channels
 freqBins = length(waveletData.wavFreq);         % number of frequency bins 
 
-%% Create marker spike trains                       
-markerInds = cell(1,nSongs);
-for songNo = 1:nSongs
-    %start index of time sample marking beginning of trial (from labjack)
-    startInd = min(find(abs(waveletData.vid{songNo})>1000000));
-    %markerInds is an integer array of marker indices (for all trials)
-    if songNo==1 || songNo==3
-        markerInds{songNo} = startInd+round(sunshineDay);
-    else
-        markerInds{songNo} = startInd+round(speedTest);
-    end
-end
-
-%% Segment all-channel EEG data
+%% Segment all-channel EEG data & wavelet data
 fprintf('Beginning Segmentation');
-% Initialize data structure components
-for songNo = 1:nSongs
+for song = 1:nSongs    
     %structure: {song}(trial x chn x trial-time)
-    waveletData.segEEG{songNo} = zeros(nTrials(songNo),nChans,sr*triallength);    
-end
-% segmentation
-for songNo = 1:nSongs    
-    fprintf('\n----song number: %i / %i----\n signal ',songNo,nSongs);
-    for trialNo = 1:nTrials(songNo)        
-        fprintf('%i -',trialNo);
+    waveletData.segEEG{song} = zeros(nTrials(song),nChans,sr*trialLength);        
+    %structure: {song}(trial x freq x chn x trial-time)
+    waveletData.segWavData{song} = zeros(nTrials(song),freqBins,nMotorChans,sr*trialLength);  
+    
+    fprintf('\nsong number: %i/%i ',song,nSongs);
+    for trial = 1:nTrials(song)        
+        fprintf('.');
         %time indices that the current trial spans (3 sec total)
-        timeSpan = markerInds{songNo}(trialNo)-(sr*triallength/2):markerInds{songNo}(trialNo)+(sr*triallength/2)-1; 
+        timeStart = (trial-1)*sr*(trialLength+1)+1;
+        timeSpan = timeStart:(timeStart+sr*trialLength-1);
+        
         %filling segment into segEEG
-        waveletData.segEEG{songNo}(trialNo,:,:) = waveletData.eeg{songNo}(:,timeSpan); %all channels       
+        waveletData.segEEG{song}(trial,:,:) = waveletData.eeg{song}(:,timeSpan); %all channels       
+        waveletData.segWavData{song}(trial,:,:,:) = waveletData.wavelet{song}(:,:,timeSpan);
     end    
 end
+fprintf('\n');
 % clear memory
-waveletData = rmfield(waveletData,'eeg');
-
-%% Segment Wavelet data & Motor EEG
-for songNo = 1:nSongs
-    %structure: {song}(trial x freq x chn x trial-time)
-    waveletData.segWavData{songNo} = zeros(nTrials(songNo),freqBins,nMotorChans,sr*triallength);  
-    %structure: {song}(trial x chn x trial-time)    
-    waveletData.segMotorEEG{songNo}= waveletData.segEEG{songNo}(:,waveletData.motorChannels,:);
-end
-% segmentation
-for songNo = 1:nSongs
-    fprintf('\n----song number: %i / %i----\n signal ',songNo,nSongs);
-    for trialNo = 1:nTrials(songNo)
-        fprintf('%i -',trialNo);
-        %time indices that the current trial spans (3 sec total)
-        timeSpan = markerInds{songNo}(trialNo)-(sr*triallength/2):markerInds{songNo}(trialNo)+(sr*triallength/2)-1; 
-        %filling segment into waveletData
-        waveletData.segWavData{songNo}(trialNo,:,:,:) = waveletData.wavelet{songNo}(:,:,timeSpan);
-    end
-end
+waveletData = rmfield(waveletData,{'eeg','vid','wavelet'});
 
 %% Saving data
-if(isfield(waveletData,'vid'))
-    waveletData = rmfield(waveletData,{'vid','motorEEG','wavelet'});
+waveletData.params.segmented = false;
+if saveBool
+    setPathTherapy(username,subname);
+    disp('Saving SEGMENTED wavelet frequency-domain data...')
+    save(strcat(subname,'_segWavData'),'waveletData','-v7.3');
+    disp('Done.');
+else
+    disp('Warning: Data not saved to disk; must pass directly');
 end
-
-fprintf('\n'); disp('Saving SEGMENTED wavelet frequency-domain data...')
-save(strcat(subname,'_segWavData'),'waveletData','-v7.3');
-disp('Done.');
 
 end
