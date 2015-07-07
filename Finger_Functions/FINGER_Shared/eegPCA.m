cee06a8d29880908d78c856a8544fa110b6c88a7
Sumner15 <28747498+sumner15@users.noreply.github.com>
2015-07-06 17:39:26 -0700

    Shared: Added a new shared repository for generic functions. As a first committ, I have added eegPCA.m, a script that performs principal component analysis. It is verified functional & complete.

---
+++ Finger_Functions/FINGER_Shared/eegPCA.m
@@ -0,0 +1,41 @@
+function PC = eegPCA(datain)
+% function PCAEnviro(username,subname,trialPower)
+% 
+% input: datain (channel x time points)
+% datain will typically contain all channels across a set of time
+% points consistent with an epoch length that surrounds the event-related
+% response to some stimulus. This will typically be an ERP or trial power 
+% calculated at a frequency of interest. 
+% 
+% eegPCA calculates the primary components for the data
+% and stores them in a structure with the fields:
+%
+% component:    (nChans x # of PCs) channel weighting for each component
+% evals:        weight of each PC
+% pcTime:       (# PCs x time points) time course of each primary component 
+% pcSpectra:    spectra of each primary component
+
+%% common vars
+[nChans,nData] = size(datain);
+
+%% calculate covariance matrix
+datain = datain - repmat(mean(datain,2),[1 nData]);
+cov = (datain*datain')./(nData-1); 
+
+%% computing PCA via eigenvalue decomposition
+[pc,evals] = eig(cov);
+% Components are listed in increasing order; here we
+% convert to descending order (most variance first)
+PC.component = pc(:,end:-1:1);
+evals = diag(evals);
+% convert to percent change
+PC.evals = 100*evals(end:-1:1)./sum(evals);
+
+%% computing time course of each PC
+PC.pcTime = PC.component'*datain;
+
+%% computing spectra of each PC
+pcSpectra = fft(PC.pcTime,[],2);
+pcSpectra = pcSpectra(1:end/2+1); %positive frequencies only
+
+end
