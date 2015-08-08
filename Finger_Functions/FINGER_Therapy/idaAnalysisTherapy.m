3d642b8abc80ad9c23d81ab89dfa8c50cda49050
Sumner15 <Sumner15@localhost>
2015-08-07 18:56:46 -0700

    Therapy: Major updates to IDA to allow subject based classification/verification (using EEG to predict motor outcome after stroke)

--- Finger_Functions/FINGER_Therapy/idaAnalysisTherapy.m
+++ Finger_Functions/FINGER_Therapy/idaAnalysisTherapy.m
@@ -40,67 +40,100 @@ C = 2;  %number of classes
 %% chosen vars
 fInterestInd = 3;           %index of frequencies of interest 
 songsInterest = [1 2];      %songs included in training set (NOT classified)
-nSongs = length(songsInterest); 
 m = 1;                      %m: size of feature space 
+outcome = 'FM';             %choose from MG,BB,FM
+plotBool = true;            %to plot or not to plot.. that is the ?
 
 fprintf('freq of interest: %iHz\n',fVec(fInterestInd));
+disp(['outcome of interest: ' outcome]);
+nSongs = length(songsInterest); 
 
 %% creating high/low groups
+switch outcome
+    case 'MG'
+        dataHead{1} = 'PercHitSD[1]';
+        dataHead{2} = 'PercHitSD[2]';  
+        cutoff = 0;
+    case 'BB'
+        dataHead{1} = 'B&B (Affected) Test [1]';
+        dataHead{2} = 'B&B (Affected) Test [2]';
+        cutoff = 1;
+    case 'FM'
+        dataHead{1} = 'FMAMA Total [1]';
+        dataHead{2} = 'FMAMA Total [3]';
+        cutoff = 4;
+end
 % outcomes: 'FMAMA Total [#]','B&B (Affected) Test [#]','PercHitSD[#]'
-condTitles = {'low delta FM','high delta FM'};
+condTitles = {['low delta ' outcome],['high delta ' outcome]};
 disp(['reset conditions to ' condTitles{1} ' & ' condTitles{2}]);
 tableSubs = therapyTextData(:,1);         % column of subject id's
-groupIndex1 = ismember(therapyTextData(1,:),'FMAMA Total [1]'); %finds group column
-groupIndex2 = ismember(therapyTextData(1,:),'FMAMA Total [2]'); %finds group column
+groupIndex1 = ismember(therapyTextData(1,:),dataHead{1}); %finds group column
+groupIndex2 = ismember(therapyTextData(1,:),dataHead{2}); %finds group column
 tableGroup1 = therapyData(:,groupIndex1); % stores group numbers
 tableGroup2 = therapyData(:,groupIndex2); % stores group numbers
 tableGroup = tableGroup2-tableGroup1;     % e.g. delta-B&B
 
-lowSubs = []; highSubs = [];          % array of indices for hi/lo subs
+% plot histogram of outcome 
+if plotBool
+    set(figure,'Position',[10 50 300 200]); 
+    hist(tableGroup)
+    xlabel(['delta ' outcome]);
+end
+
+% separate high and low subs
+lowSubs = []; highSubs = [];         % array of indices for hi/lo subs
 for currentSub = 1:nSubs
     subname = subjects{currentSub};      
     tableSubInd = ismember(tableSubs,subname); %index of sub in table
-    group = tableGroup(tableSubInd); %current sub's group level
-    if group < nanmean(tableGroup(2:end-3))
-%     if group <= 10
-        lowSubs = [lowSubs currentSub];
-    elseif group > nanmean(tableGroup(2:end-3))
-%     elseif group > 10
+    group = tableGroup(tableSubInd); %current sub's group level    
+    if group < cutoff
+        lowSubs = [lowSubs currentSub];    
+    elseif group >= cutoff
         highSubs = [highSubs currentSub];
     else 
         warning([subname ': group level is not properly defined']);
     end
 end
 
-%% Averaging/Selecting across dimensions (reduction)
+% create array of subject classes 
+allSubClass = NaN(nSubs,1);      
+allSubClass(lowSubs) = 0; 
+allSubClass(highSubs) = 1;
+
+% rearrange for NaN subjects
+nSubs = length(lowSubs)+length(highSubs); %update nSubs to exclude NaNs
+subClass = allSubClass(~isnan(allSubClass));  % throws out NaN subjects 
+trialPowerDB(isnan(allSubClass),:,:,:,:,:) = [];
+
+%% Resisizing data for classification
 disp('resizing data for classification...')
-% take a subset of songs into account 
-reshapedPower = NaN([nSubs nWins nFreqs nChans nTrials*nSongs]);
+% take a subset of songs into account (combining them into many trials)
+reshapedPower = NaN([nSubs nWins nFreqs nChans nTrials]);
 for song = songsInterest
     ind = (song-1)*nTrials+1:song*nTrials;
     % (sub,win,freq,chan,trial*cond)
     reshapedPower(:,:,:,:,ind) = squeeze(trialPowerDB(:,song,:,:,:,:));
 end
-% average across subs by class (making the subs our condition!)
-interSubPower(1,:,:,:,:) = nanmean(reshapedPower(lowSubs ,:,:,:,:),1); 
-interSubPower(2,:,:,:,:) = nanmean(reshapedPower(highSubs,:,:,:,:),1); %now (cond,win,freq,chan,trial)
+nTrials = nTrials*nSongs;
 % flatten at frequency of interest
-interSubPower = squeeze(nanmean(interSubPower(:,:,fInterestInd,:,:),3)); %(cond,win,chan,trial)
+interSubPower = squeeze(nanmean(reshapedPower(:,:,fInterestInd,:,:),3)); %(sub,win,chan,trial)
 
 %% organize into correct size (nTrials*nClasses x nChans*nWins)
 % this is a tall matrix of flattened windows/trials/classes of width channels
-Train = NaN(C*nTrials , nChans*nWins);
-Group = NaN(C*nTrials , 1);
-for class = 1:C
+Train = NaN(nTrials*nSubs , nChans*nWins);  %training data set
+Group = NaN(nTrials*nSubs , 1);             %corresponding class vector
+subNum = NaN(nTrials*nSubs, 1);
+for subject = 1:nSubs  
    for trial = 1:nTrials
-       verticalIndex = (class-1)*nTrials + trial;
+       verticalIndex = (subject-1)*nTrials + trial;
+       subNum(verticalIndex) = subject;
        % vertical vector of class labels corresponding to 'Train'
-       Group(verticalIndex) = class-1;
+       Group(verticalIndex) = subClass(subject);
        for window = 1:nWins
            horizontalIndex = (window-1)*nChans+1:(window-1)*nChans+nChans;
            % Training data 
            Train(verticalIndex,horizontalIndex) = ...
-               squeeze(interSubPower(class,window,:,trial));
+               squeeze(interSubPower(subject,window,:,trial));
        end
    end
 end
@@ -108,6 +141,7 @@ end
 nanInds = max(isnan(Train),[],2);
 Train(nanInds,:) = [];
 Group(nanInds) = [];
+subNum(nanInds) = [];
 
 %% choosing analysis based on data size
 ida = size(Train,2)*10 < size(Train,1);
@@ -149,48 +183,52 @@ for class = 1:C
 end
 
 %% plotting channel weighting & feature space
-% set(figure,'Position',figSize); 
-% suptitle([methodString ' topography :: ' condTitles{1}...
-%     ' vs. ' condTitles{2} ', ' num2str(fVec(fInterestInd))... 
-%     ' Hz, ' num2str([length(lowSubs) length(highSubs)]) ' subject(s)']);
-% for window = 1:5
-%     subplot(3,5,window)
-%     fftWins = (window-1)*3+1:(window*3);
-%     corttopo(mean(T(:,fftWins),2),hm,'drawElectrodes',0)
-%     caxis([-1 1]); 
-% end
-% 
-% % class power
-% subplot(3,5,6:10)
-% plot(t,classPower','LineWidth',4)
-% legend(condTitles{1},condTitles{2});
-% title('time series representation by class')
-% xlabel('time(sec relative to target time)'); ylabel('Power (dB)')
-% 
-% % feature space
-% subplot(3,5,11:15)
-% for class = 1:C   
-%     ph = plot(FeatureTrain(Group==class-1,1),zeros(size(FeatureTrain(Group==class-1,1))),'o');      
-%     hold on
-% end
-% title([methodString ' feature space']);
-% legend(condTitles{1},condTitles{2});
+if plotBool
+    set(figure, 'Position',figSize); 
+    suptitle([methodString ' topography :: ' condTitles{1}...
+        ' vs. ' condTitles{2} ', ' num2str(fVec(fInterestInd))... 
+        ' Hz, ' num2str([length(lowSubs) length(highSubs)]) ' subject(s)']);
+    for window = 1:5
+        subplot(3,5,window)
+        fftWins = (window-1)*3+1:(window*3);
+        corttopo(mean(T(:,fftWins),2),hm,'drawElectrodes',0)
+        caxis([-1 1]); 
+    end
+
+    % class power
+    subplot(3,5,6:10)
+    plot(t,classPower','LineWidth',4)
+    legend(condTitles{1},condTitles{2});
+    title('time series representation by class')
+    xlabel('time(sec relative to target time)'); ylabel('Power (dB)')
+
+    % feature space
+    subplot(3,5,11:15)
+    for class = 1:C   
+        ph = plot(FeatureTrain(Group==class-1,1),zeros(size(FeatureTrain(Group==class-1,1))),'o');      
+        hold on
+    end
+    title([methodString ' feature space']);
+    legend(condTitles{1},condTitles{2});
+end
 
 %% commence classification testing
 testClassify = input('Would you like to classify? (type y or n): ','s');
-nSubs = length(lowSubs)+length(highSubs);
 
-if strcmp(testClassify,'y') && cpca                
-    nCorrect = 0;
-    for trial = 1:length(Group)        
-        leaveOut = trial;
+if strcmp(testClassify,'y') && cpca     
+    fprintf('Classifying ');
+    correct = zeros(1,nSubs);
+    confidence = zeros(1,nSubs);
+    for subject = 1:nSubs
+        fprintf('.');
+        leaveOut = find(subNum==subject);
 
-        % create training set (leaves one out)
+        % create training set for validation (leaves one out)
         trainValid = Train; 
         trainValid(leaveOut,:)=[];
         trainValidGroup = Group;
         trainValidGroup(leaveOut) = [];
-        % create validate point(the one left out)        
+        % create validation set (the one left out)        
         valid = Train(leaveOut,:);
         validGroup = Group(leaveOut,:);
 
@@ -204,12 +242,15 @@ if strcmp(testClassify,'y') && cpca
         % classify our left out trial
         classPredicted = classify(FeatureValid,FeatureTrain,trainValidGroup,'linear', ...
                  'empirical');
+        subClassPredicted = mean(classPredicted);
+        fprintf('%i',round(subClassPredicted))
 
         % count how many we got right
-        if classPredicted == validGroup
-            nCorrect = nCorrect+1;
+        if round(subClassPredicted) == subClass(subject)            
+            correct(subject) = 1;
         end
+        confidence(subject) = (1-abs(subClassPredicted-round(subClassPredicted)))*100;
     end
-    percCorrect = nCorrect/length(Group)*100;
-    fprintf('Classifaction Accuracy: %3.2f \n',percCorrect);    
+    percCorrect = mean(correct)*100;
+    fprintf('\nClassifaction Accuracy: %i/%i = %3.2f%% \n',sum(correct),nSubs,percCorrect);    
 end
