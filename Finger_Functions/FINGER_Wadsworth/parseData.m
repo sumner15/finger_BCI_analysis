3e707c04b1b1e045e6b46f1c5db181c7eead65c5
Sumner15 <Sumner15@localhost>
2017-01-18 17:10:52 -0800

    Updated Wadsworth data parsing to look at BRAA data.

--- Finger_Functions/FINGER_Wadsworth/parseData.m
+++ Finger_Functions/FINGER_Wadsworth/parseData.m
@@ -1,17 +1,20 @@
 %calls datToMat to parse data from BCI2000 Wadsworth BCI study
 clear
-startDir = pwd;
 
-subname = 'combForces';
-runNames = {'move only','force only'};
+subname = 'BRAA';
+session = 'BRAA003';
+runNames = {'1','2','3','4','5','6','7','8'};
 runs = length(runNames);
 
 try 
     if ispc
-        cd 'C:\Users\Sumner\Dropbox\UCI RESEARCH\FINGER\FINGER_wadsworth\DenniShare\force_transducer_test_data\'
+        cd 'C:\Users\Sumner\Dropbox\UCI RESEARCH\FINGER\FINGER_wadsworth\Data\'
     else
-        cd '/Users/Sum/Dropbox/UCI RESEARCH/FINGER/FINGER_wadsworth/DenniShare/force_transducer_test_data/'
+        cd '/Users/Sum/Dropbox/UCI RESEARCH/FINGER/FINGER_wadsworth/Data/'        
     end
+    startDir = pwd;
+    cd(subname)
+    cd(session)
     data = datToMat(subname,runs);
     cd(startDir)
 catch me
@@ -27,21 +30,26 @@ for run = 1:runs
                              
     %normalizing data for easier plotting
     data.pos{run}(data.pos{run}>2000) = 0;    
+    data.posNorm{run} = data.pos{run}/max(data.pos{run}(:));
     data.forceNorm{run} = data.force{run}/max(data.force{run}(:));    
 end
 
-%% plot force traces
+%% plot force traces and correlation coeffs for all runs
 close all
 
-set(figure,'Position',[100 50 400*runs 500])
+set(figure,'Position',[50 0 300*runs 500])
 for run = 1:runs   
     subplot(2,runs,run)
     sample = 1:size(data.force{run},1);
     hold on
-    plot(sample,data.pos{run}(:,1),'--',...
-         sample,data.pos{run}(:,2),'--','LineWidth',2);
-    plot(sample,data.force{run}(:,1),...                  
-         sample,data.force{run}(:,2),'LineWidth',3);                  
+%     plot(sample,data.pos{run}(:,1),'--',...
+%          sample,data.pos{run}(:,2),'--','LineWidth',2);
+    plot(sample,data.posNorm{run}(:,1),'--',...
+         sample,data.posNorm{run}(:,2),'--','LineWidth',2);
+%     plot(sample,data.force{run}(:,1),...                  
+%          sample,data.force{run}(:,2),'LineWidth',3);  
+    plot(sample,data.forceNorm{run}(:,1),...                  
+         sample,data.forceNorm{run}(:,2),'LineWidth',3);  
     set(gca,'xtick',[]); 
     xlabel('time')
     ylabel('Force')
@@ -58,3 +66,55 @@ for run = 1:runs
     yticklabels({'Pos1','Pos2','F1','F2'})    
     colormap autumn
 end
+
+%% plot first run data traces (raw and normalized)
+run = 1;
+sample = 1:size(data.force{run},1);
+
+set(figure,'Position',[50 0 1600 700])
+subplot(121)    
+hold on
+plot(sample,data.pos{run}(:,1),'--',...
+     sample,data.pos{run}(:,2),'--','LineWidth',2);    
+plot(sample,data.force{run}(:,1),...                  
+     sample,data.force{run}(:,2),'LineWidth',3);  
+ set(gca,'xtick',[]); 
+xlabel('time')
+ylabel('raw amplitude')
+legend('Pos1','Pos2','F1','F2','Location','SouthWest')
+title(['Run ' num2str(run) ': ' runNames{run}])
+     
+subplot(122)
+hold on
+plot(sample,data.posNorm{run}(:,1),'--',...
+     sample,data.posNorm{run}(:,2),'--','LineWidth',2);
+plot(sample,data.forceNorm{run}(:,1),...                  
+     sample,data.forceNorm{run}(:,2),'LineWidth',3); 
+     set(gca,'xtick',[]); 
+xlabel('time')
+ylabel('force & position (normalized)')
+legend('Pos1','Pos2','F1','F2','Location','SouthWest')
+title(['Run ' num2str(run) ': ' runNames{run}])
+
+%% plot results separately
+run = 1;
+sample = 10:size(data.force{run},1);
+time = sample/256;
+
+set(figure,'Position',round((get(groot, 'Screensize' )+59)*0.833))
+subplot(411)    
+plot(time,data.pos{run}(10:end,1),'--','LineWidth',2);  
+xlabel('time (s)')
+ylabel('Pos 1')
+subplot(412)    
+plot(time,data.pos{run}(10:end,2),'--','LineWidth',2);   
+xlabel('time (s)')
+ylabel('Pos 2')
+subplot(413)    
+plot(time,data.force{run}(10:end,1),'LineWidth',3);  
+xlabel('time (s)')
+ylabel('Force 1')
+subplot(414)    
+plot(time,data.force{run}(10:end,2),'LineWidth',3); 
+xlabel('time (s)') 
+ylabel('Force 2')   
