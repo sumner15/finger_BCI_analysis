e8c573c3880f84be8a86c003cfb271713538ec1b
Sumner15 <Sumner15@localhost>
2017-06-07 10:13:05 -0700

    plotOverSession generalizes the ability to plot a single measure across sessions for all subjects. Moved the prototype functions out of their respective scripts.

---
+++ Finger_Functions/FINGER_Wadsworth/plotOverSession.m
@@ -0,0 +1,54 @@
+function plotOverSession(data,label,subjects)
+% This function plots a single measure across all sessions for individual
+% components. A second plot shows the change in this measure compared to
+% baseline (useful for clinical data and basic performance measures)
+%
+% data is a {nSubs x 1} cell array where each cell should contain a 1 x
+% nSessions array of measurement data
+% 
+% label is a string containing the measure description/label
+%
+% subjects is a {1 x nSubs} cell array where each cell contains a string
+% subject identifier, e.g. 'NORS'
+
+    %% set up            
+    if size(data{1},2)==5 %if this is clinical data
+        sessions = [0 1 3 10 12];
+    elseif size(data{1},2)==12 %if this is another measure
+        sessions = 1:12;
+    end
+    
+    nSubs = length(subjects);
+
+    %% plot measure 
+    set(figure,'Position',[150 20 1400 500]);
+    subplot(121)
+    hold on
+    set(0,'defaultlinelinewidth',2.5)
+    for sub = 1:nSubs
+        plot(sessions,data{sub},'-o')        
+    end    
+    ylabel(label)
+
+    subplot(122)
+    hold on
+    for sub = 1:nSubs
+        plot(sessions,data{sub}-data{sub}(1),'-o')
+    end    
+    ylabel(['\delta ' label])    
+
+    %% set type and legend
+    set(findall(gcf,'-property','FontSize'),'FontSize',14)
+    sessionTitles = {'BL','Phase 1','','','Phase 2','','','','','',...
+        'Phase 3','','end'};
+    for sub = 1:2
+        subplot(1,2,sub)  
+        xlim([-1 13])
+        xticks(0:12)
+        xticklabels(sessionTitles)                   
+        xlabel('session')    
+        xtickangle(45)    
+    end
+    leg1 = legend(subjects,'location','best');
+        set(leg1,'FontSize',10)
+end
