65f538ea798c1bc1f7b060d9894005c7cfba9918
DarthSumner <28747498+sumner15@users.noreply.github.com>
2017-06-21 22:32:22 -0700

    Added force trace reading & plotting capabilities (tested on CHEA only)

--- Finger_Functions/FINGER_Wadsworth/getAllMovementTraces.m
+++ Finger_Functions/FINGER_Wadsworth/getAllMovementTraces.m
@@ -5,16 +5,16 @@
 % getERD(), getHitRate(), getMaxF()
 
 
-clear; clc; close all
-% subjects = {'CHEA','MAUA','VANT','TRUL','PHIC','RAZT','MCCL','HATA'};   
+clear; clc; close all 
 subjects = {'CHEA','VANT','TRUL','RAZT','MCCL','PHIC','HATA','MAUA'};
+subjects = {'CHEA'};   
 nSubs = length(subjects);
 startDir = pwd;
 
 %% set up cell arrays for data
 yellow = 1; blue = 2;
 index = 1; middle = 2; both = 3;
-[tracesYellow, tracesBlue] = deal(cell(nSubs,3));
+[tracesYellow, tracesBlue, tausYellow, tausBlue] = deal(cell(nSubs,3));
 
 %% for each subject 
 for sub = 1:nSubs              
@@ -23,7 +23,8 @@ for sub = 1:nSubs
     subID = subjects{sub};      
     
     for finger = index:both
-        [tracesY, tracesB] = deal(cell(1,3)); %reset on each finger
+        [tracesY, tracesB, tausY, tausB] =...
+            deal(cell(1,3)); %reset on each finger
         
         for session = 10:12            
             %% load data into mat format
@@ -34,11 +35,15 @@ for sub = 1:nSubs
 
             %% extract movement data            
             tracesY{session-9} = getMoveTrace(data, session, yellow, finger);   
-            tracesB{session-9} = getMoveTrace(data, session, blue, finger);            
+            tracesB{session-9} = getMoveTrace(data, session, blue, finger);   
+            tausY{session-9} = getForceTrace(data, session, yellow, finger);
+            tausB{session-9} = getForceTrace(data, session, blue, finger);           
             
         end
         tracesYellow{sub,finger} = [tracesY{1}; tracesY{2}; tracesY{3}];
         tracesBlue{sub,finger} = [tracesB{1}; tracesB{2}; tracesB{3}];        
+        tausYellow{sub, finger} = [tausY{1}; tausY{2}; tausY{3}];
+        tausBlue{sub, finger} = [tausB{1}; tausB{2}; tausB{3}];        
     end
 end
 
@@ -49,7 +54,8 @@ saveBool = input('Would you like to save the results? (y/n): ','s');
 if strcmp(saveBool,'y')
     dataDirectory();
     disp('Saving to traces.mat')
-    save('traces','tracesYellow','tracesBlue','subjects','nSubs');
+    save('traces','tracesYellow','tracesBlue','tausYellow','tausBlue',...
+        'subjects','nSubs');
     cd(startDir)
 end
 
--- Finger_Functions/FINGER_Wadsworth/plotYvsB.m
+++ Finger_Functions/FINGER_Wadsworth/plotYvsB.m
@@ -1,49 +1,62 @@
-function plotYvsB(tracesYellow,tracesBlue)
+function plotYvsB()
     %% load data
-    disp('plotting all phase 3 movement data')
-    disp('         ||||||||||||||||||||||||')
+    disp('plotting all phase 3 movement data')    
     fprintf('progress ')
+    
     if nargin==0
         startDir = dataDirectory();
-        load traces.mat
-        cd(startDir);
-    end            
+        data = load('traces.mat');
+        cd(startDir);      
+    end
+    
+    %% call plotting functions
+    plotAway(data.tracesYellow, data.tracesBlue, data)
+    plotAway(data.tausYellow, data.tausBlue, data)
 
-    %% plot data
+%% plot data
+function plotAway(yellowData, blueData, data)
+    disp('         ||||||||||||||||||||||||')
     set(figure,'Position',[100 20 2000 1100]);            
     for finger = 1:3        
-        for sub = 1:nSubs
+        for sub = 1:data.nSubs
             % create time vector
-            t = (1:size(tracesYellow{sub,finger},2))/256;
+            t = (1:size(yellowData{sub,finger},2))/256;
             
             % set subplot
-            subplot(3,nSubs,nSubs*(finger-1)+sub)
+            subplot(3,data.nSubs,data.nSubs*(finger-1)+sub)
             fprintf('|')
             
-            for i = 1:size(tracesYellow{sub,finger},1)
+            % normalize data to max of 1
+            yellowData{sub,finger} = ...
+                yellowData{sub,finger}./max(yellowData{sub,finger}(:));
+            blueData{sub,finger} = ...
+                blueData{sub,finger}./max(blueData{sub,finger}(:));
+            
+            for i = 1:size(yellowData{sub,finger},1)
                 % plot all yellow traces
-                hY = plot(t,tracesYellow{sub,finger}(i,:)',...
+                hY = plot(t,yellowData{sub,finger}(i,:)',...
                     'color', [1 0.85 0], 'linewidth', 1.5);
                 hY.Color(:,4) = 0.3;
                 hold on            
                 % plot mean of yellow traces
-                plot(t,mean(tracesYellow{sub,finger},1),...
+                plot(t,mean(yellowData{sub,finger},1),...
                     'color', [1 0.85 0], 'linewidth', 2);
                 % plot all blue traces
-                hB = plot(t,tracesBlue{sub,finger}(i,:)',...
+                hB = plot(t,blueData{sub,finger}(i,:)',...
                     'color', [0 0.4 0.65], 'linewidth', 1.5);
                 hB.Color(:,4) = 0.3;
                 % plot mean of blue traces
-                plot(t,mean(tracesBlue{sub,finger},1),...
+                plot(t,mean(blueData{sub,finger},1),...
                     'color', [0 0.4 0.65], 'linewidth', 2);
             end
             
-            setType(t,sub,subjects,finger)
+            setType(t,sub,data.subjects,finger)
         end
     end    
     fprintf('\n')
 end
 
+%% function to set type
 function setType(t,sub,subjects,finger)
     set(findall(gcf,'-property','FontSize'),'FontSize',14)
     fingerStrings = {'index','middle','both'};
@@ -52,9 +65,11 @@ function setType(t,sub,subjects,finger)
         ylabel(fingerStrings(finger))
     end
     
-    axis([0 t(end) 0 900])
+    xlim([0 t(end)])
     xticks([0 0.5 1.0 1.5])
     xticklabels({'0','0.5','1.0','1.5'})
-    yticks([50 850])
-    yticklabels({'flexed','extended'})
+    yticks([-0.95 0.00 0.95])
+    yticklabels({'flexed','','extended'})
+end
+
 end
