2ed2919c4bbba8d0eb2e7a8fac935e2f4148a806
DarthSumner <28747498+sumner15@users.noreply.github.com>
2017-06-23 17:41:01 -0700

    Added measures of individuation to plotting

--- Finger_Functions/FINGER_Wadsworth/plotYvsB.m
+++ Finger_Functions/FINGER_Wadsworth/plotYvsB.m
@@ -10,13 +10,20 @@ function plotYvsB()
     end
     
     %% call plotting functions
-    plotAway(data.tracesYellow1, data.tracesBlue1,...
-             data.tracesYellow2, data.tracesBlue2, data)
-    plotAway(data.tausYellow1, data.tausBlue1, ...
-             data.tausYellow2, data.tausBlue2, data)
+    % plot movement traces & torque traces for both fingers
+    plotAway(data, data.tracesYellow1, data.tracesBlue1,...
+                   data.tracesYellow2, data.tracesBlue2)
+    plotAway(data, data.tausYellow1, data.tausBlue1, ...
+                   data.tausYellow2, data.tausBlue2)
+    % plot movement traces & torque traces for the target finger only
+    plotAway(data, data.tracesYellow1, data.tracesBlue1)
+    plotAway(data, data.tausYellow1, data.tausBlue1)
+    % plot individuation traces 
+    plotAway(data, data.posIndYellow, data.posIndBlue)
+    plotAway(data, data.tauIndYellow, data.tauIndBlue)
 
 %% plot data
-function plotAway(yellowData, blueData, orangeData, purpleData, data)
+function plotAway(data, yellowData, blueData, orangeData, purpleData)
     fprintf('         ')
     set(figure,'Position',[100 20 2000 1100]);            
     for finger = 1:3        
@@ -28,35 +35,40 @@ function plotAway(yellowData, blueData, orangeData, purpleData, data)
             subplot(3,data.nSubs,data.nSubs*(finger-1)+sub)
             fprintf('|')
             
-            % normalize data to max of 1
-            maxY = max(abs(yellowData{sub,finger}(:)));
-            maxB = max(abs(blueData{sub,finger}(:)));
-            maxO = max(abs(orangeData{sub,finger}(:)));
-            maxP = max(abs(purpleData{sub,finger}(:)));
-            maxVal = max([maxY maxB maxO maxP]);
+            % normalize data to max of 1            
+            if exist('orangeData','var') && exist('purpleData','var')
+                maxVal = getMaxVal(sub,yellowData,blueData,orangeData,purpleData);
+                orangeData{sub,finger} = orangeData{sub,finger}./maxVal;
+                purpleData{sub,finger} = purpleData{sub,finger}./maxVal;
+            else
+                maxVal = getMaxVal(sub,yellowData,blueData);
+            end
             yellowData{sub,finger} = yellowData{sub,finger}./maxVal;
-            blueData{sub,finger} = blueData{sub,finger}./maxVal;
-            orangeData{sub,finger} = orangeData{sub,finger}./maxVal;
-            purpleData{sub,finger} = purpleData{sub,finger}./maxVal;
+            blueData{sub,finger} = blueData{sub,finger}./maxVal;   
+         
             
+            % HERE COMES THE PLOTTING
             for i = 1:size(yellowData{sub,finger},1)
                 % plot all yellow traces
                 hY = plot(t,yellowData{sub,finger}(i,:)',...
                     'color', [1 0.85 0], 'linewidth', 1.5); 
                 hY.Color(:,4) = 0.3;
                 hold on            
-               % plot all orange traces (secondary finger)
-                hO = plot(t,orangeData{sub,finger}(i,:)',...
-                    'color', [1 0.4 0.4], 'lineWidth', 1.5);                
-                hO.Color(:,4) = 0.3;
-               % plot all blue traces
+                % plot all blue traces
                 hB = plot(t,blueData{sub,finger}(i,:)',...
                     'color', [0 0.4 0.65], 'linewidth', 1.5);
                 hB.Color(:,4) = 0.3;
-                % plot all purple traces (secondary finger)
-                hP = plot(t,purpleData{sub,finger}(i,:)',...
-                    'color', [0.5 0.2 0.6], 'lineWidth', 1.5);
-                hP.Color(:,4) = 0.3;                
+                
+                if exist('orangeData','var') && exist('purpleData','var')
+                    % plot all orange traces (secondary finger)
+                    hO = plot(t,orangeData{sub,finger}(i,:)',...
+                        'color', [1 0.4 0.4], 'lineWidth', 1.5);                
+                    hO.Color(:,4) = 0.3;
+                    % plot all purple traces (secondary finger)
+                    hP = plot(t,purpleData{sub,finger}(i,:)',...
+                        'color', [0.5 0.2 0.6], 'lineWidth', 1.5);
+                    hP.Color(:,4) = 0.3;                
+                end
             end
             
             % set axis limits based on data 
@@ -64,8 +76,7 @@ function plotAway(yellowData, blueData, orangeData, purpleData, data)
                 axisLims = [0 t(end) -1 1];
             else
                 axisLims = [0 t(end) 0 1];
-            end
-            
+            end            
             % set axes type
             setType(sub,data.subjects,finger,axisLims)
         end
@@ -87,6 +98,22 @@ function setType(sub,subjects,finger,axisLims)
     xticklabels({'0','0.5','1.0','1.5'})
     yticks(axisLims(3:4))
     yticklabels({'flexed','extended'})
+end
+
+%% function to get the maximum data value for this subject, across fingers
+function maxVal = getMaxVal(sub,yellowData,blueData,orangeData,purpleData)
+    [maxVal,maxY,maxB,maxO,maxP] = deal(0);
+    for finger = 1:3
+        maxY = max([maxY; max(abs(yellowData{sub,finger}(:)))]);
+        maxB = max([maxB; max(abs(blueData{sub,finger}(:)))]);
+        if exist('orangeData','var') && exist('purpleData','var')
+            maxO = max([maxO; max(abs(orangeData{sub,finger}(:)))]);
+            maxP = max([maxP; max(abs(purpleData{sub,finger}(:)))]);
+            maxVal = max([maxVal; max([maxY maxB maxO maxP])]);
+        else
+            maxVal = max([maxVal max([maxY maxB])]);
+        end         
+    end
 end
 
 end
