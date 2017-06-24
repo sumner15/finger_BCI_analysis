d6cd573bcc78897ac64a775737bda1ef4f8bc507
DarthSumner <28747498+sumner15@users.noreply.github.com>
2017-06-24 16:34:31 -0700

    Updated getLatency to use the same check procedures as the trace data scripts. Added a generic function to get continuous data w/in sessions (across runs).

--- Finger_Functions/FINGER_Wadsworth/getForceTrace.m
+++ Finger_Functions/FINGER_Wadsworth/getForceTrace.m
@@ -8,13 +8,6 @@
 function [traces, otherFinger, individuation] = ...
     getForceTrace(dataIn, session, target, finger)
 
-% phase 2 has no latency results
-if session >= 4 && session <=9
-    [traces, otherFinger, individuation] = deal(NaN);
-    warning(['session ' num2str(session) ' is not a movement session'])
-    return
-end
-
 % default to yellow target
 if ~exist('target','var')
     target = 1;
@@ -27,27 +20,11 @@ if ~exist('finger','var')
     warning('assuming you wanted the index finger results')
 end
 
-%% put needed data in continuous format
-nRuns = length(dataIn.state);
-[f1a, f1b, f2a, f2b, pos1, pos2, moveTarget, EEGTarget, taskState, t, result] = deal([]);
+%% get data into continuous format
+[f1a, f1b, f2a, f2b, pos1, pos2, ...
+ moveTarget, EEGTarget, ~, ~, result, nTrials, goInds] = ...
+    getContinuousData(dataIn);
 
-for run = 1:nRuns
-    % note:
-    % move target (cursorColors)  0-none 1-index 2-middle 3-both
-    % EEG target (targetCode) 0-none 1-yellowSquare 2-blueSquare
-    % taskState 0-none 1-EEGSquare 2-preMovement 3-moveCircles 4-feedback
-    pos1 = [pos1 ; double(dataIn.state{1,run}.FRobotPos1)];
-    pos2 = [pos2 ; double(dataIn.state{1,run}.FRobotPos2)];
-    f1a = [f1a ; double(dataIn.state{1,run}.FRobotForceF1a)];
-    f1b = [f1b ; double(dataIn.state{1,run}.FRobotForceF1b)];
-    f2a = [f2a ; double(dataIn.state{1,run}.FRobotForceF2a)];
-    f2b = [f2b ; double(dataIn.state{1,run}.FRobotForceF2b)];
-    moveTarget = [moveTarget ; double(dataIn.state{1,run}.CursorColors)];
-    EEGTarget = [EEGTarget ; double(dataIn.state{1,run}.TargetCode)];
-    taskState = [taskState ; double(dataIn.state{1,run}.TaskState3)];
-    t = [t ; double(dataIn.state{1,run}.SourceTime)];    
-    result = [result ; double(dataIn.state{1,run}.CursorResult)];
-end
 tau1 = -1.55*f1a - 2.82*f1b + 7.87;
 tau2 = -1.55*f2a - 2.82*f2b + 7.87;
 
@@ -57,28 +34,8 @@ if session<= 3
     EEGTarget = target*ones(size(EEGTarget));
 end
 
-%% fix time vector to be monotonically increasing
-for i = 2:length(t)
-    if t(i) < t(i-1)
-        t(i:end) = t(i:end)+t(i-1)-t(i);
-    end
-end
-t = t-t(1);
-
-%% find movement cue (task state -> 3)
-goCue = zeros(size(taskState));
-for sample = 2:length(taskState)
-    % if, at this sample, we gave 'go' cue 
-    if taskState(sample)==3 && taskState(sample-1)~=3 
-        goCue(sample) = 1;
-    end
-end
-goInds = find(goCue==1);
-goInds = [goInds ; length(t)];
-nTrials = length(goInds)-1;
-
 %% get movement traces
-samplesInTrace = 400;
+
 [traces, otherFinger, individuation] = deal(NaN(nTrials,samplesInTrace));
 
 for trial = 1:nTrials   
@@ -132,4 +89,6 @@ for trial = 1:nTrials
 %                     transpose((tauDiff1-tauDiff2)./(tauDiff1+tauDiff2));
         end                             
     end
+end
+
 end
