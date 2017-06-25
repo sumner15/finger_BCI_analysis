f7742951494b0b7b94d79c9a4d6801e955397281
DarthSumner <28747498+sumner15@users.noreply.github.com>
2017-06-24 18:51:25 -0700

    getLatency extended to include force measures of performance

--- Finger_Functions/FINGER_Wadsworth/performancePlots.m
+++ Finger_Functions/FINGER_Wadsworth/performancePlots.m
@@ -22,7 +22,10 @@ function loadAndPlot(subjects)
     [ERDp, ERDR2, hitRateEEG, hitRateRobot, ...
         latYI, latBI, latYM, latBM, latYB, latBB,...
         maxPYI, maxPBI, maxPYM, maxPBM, maxPYB, maxPBB,...
-        maxVYI, maxVBI, maxVYM, maxVBM, maxVYB, maxVBB]...
+        maxVYI, maxVBI, maxVYM, maxVBM, maxVYB, maxVBB,...
+        maxTYI, maxTBI, maxTYM, maxTBM, maxTYB, maxTBB,...
+        minTYI, minTBI, minTYM, minTBM, minTYB, minTBB,...
+        latMaxTYI, latMaxTBI, latMaxTYM, latMaxTBM, latMaxTYB, latMaxTBB]...
          = deal(cell(nSubs,1));
     
     for sub = 1:nSubs
@@ -34,31 +37,46 @@ function loadAndPlot(subjects)
        hitRateRobot{sub} = subData.hitRateRobot;
 
        latYI{sub} = subData.latencyYI;      
-       latBI{sub} = subData.latencyBI; 
-      
+       latBI{sub} = subData.latencyBI;       
        latYM{sub} = subData.latencyYM;
-       latBM{sub} = subData.latencyBM;
-       
+       latBM{sub} = subData.latencyBM;       
        latYB{sub} = subData.latencyYB;
        latBB{sub} = subData.latencyBB;
        
        maxPYI{sub} = subData.maxPYI;      
-       maxPBI{sub} = subData.maxPBI; 
-      
+       maxPBI{sub} = subData.maxPBI;       
        maxPYM{sub} = subData.maxPYM;
-       maxPBM{sub} = subData.maxPBM;
-       
+       maxPBM{sub} = subData.maxPBM;       
        maxPYB{sub} = subData.maxPYB;
        maxPBB{sub} = subData.maxPBB;
        
        maxVYI{sub} = subData.maxVYI;      
-       maxVBI{sub} = subData.maxVBI; 
-      
+       maxVBI{sub} = subData.maxVBI;       
        maxVYM{sub} = subData.maxVYM;
-       maxVBM{sub} = subData.maxVBM;
-       
+       maxVBM{sub} = subData.maxVBM;       
        maxVYB{sub} = subData.maxVYB;
        maxVBB{sub} = subData.maxVBB;
+       
+       maxTYI{sub} = subData.maxTYI;      
+       maxTBI{sub} = subData.maxTBI;       
+       maxTYM{sub} = subData.maxTYM;
+       maxTBM{sub} = subData.maxTBM;       
+       maxTYB{sub} = subData.maxTYB;
+       maxTBB{sub} = subData.maxTBB;
+       
+       minTYI{sub} = subData.minTYI;      
+       minTBI{sub} = subData.minTBI;       
+       minTYM{sub} = subData.minTYM;
+       minTBM{sub} = subData.minTBM;       
+       minTYB{sub} = subData.minTYB;
+       minTBB{sub} = subData.minTBB;
+       
+       latMaxTYI{sub} = subData.latMaxTYI;      
+       latMaxTBI{sub} = subData.latMaxTBI;       
+       latMaxTYM{sub} = subData.latMaxTYM;
+       latMaxTBM{sub} = subData.latMaxTBM;       
+       latMaxTYB{sub} = subData.latMaxTYB;
+       latMaxTBB{sub} = subData.latMaxTBB;
     end
 
     %% plot results over session
@@ -84,7 +102,7 @@ function loadAndPlot(subjects)
     subplot(fingers, measures, 6)
     plotOverSession(latBB, 'both latency w/ ERS (ms)', subjects)
     
-    %% plot phase 3 results
+    %% plot phase 3 movement results
     set(figure,'Position',[100 20 2000 1100]);      
     fingers = 3; 
     measures = 3;
@@ -112,5 +130,32 @@ function loadAndPlot(subjects)
     plotPhase3(maxVYM, maxVBM, 'middle max vel', subjects)
     subplot(fingers, measures, 2*measures+3)
     plotPhase3(maxVYB, maxVBB, 'both max vel', subjects, true)
+    
+    %% plot phase 3 force results
+    set(figure,'Position',[100 20 2000 1100]);   
+    
+    % plot max torque results (first row)
+    subplot(fingers, measures, 1)
+    plotPhase3(maxTYI, maxTBI, 'index max \tau', subjects)
+    subplot(fingers, measures, measures+1)
+    plotPhase3(maxTYM, maxTBM, 'middle max \tau', subjects)
+    subplot(fingers, measures, 2*measures+1)
+    plotPhase3(maxTYB, maxTBB, 'both max \tau', subjects)
+    
+    % plot min torque results (second row)
+    subplot(fingers, measures, 2)
+    plotPhase3(minTYI, minTBI, 'index min \tau', subjects)
+    subplot(fingers, measures, measures+2)
+    plotPhase3(minTYM, minTBM, 'middle min \tau', subjects)
+    subplot(fingers, measures, 2*measures+2)
+    plotPhase3(minTYB, minTBB, 'both min \tau', subjects)
+    
+    % plot latency to max torque (third row)
+    subplot(fingers, measures, 3)
+    plotPhase3(latMaxTYI, latMaxTBI, 'index t to max \tau (ms)', subjects)
+    subplot(fingers, measures, measures+3)
+    plotPhase3(latMaxTYM, latMaxTBM, 'middle t to max \tau (ms)', subjects)
+    subplot(fingers, measures, 2*measures+3)
+    plotPhase3(latMaxTYB, latMaxTBB, 'both t to max \tau (ms)', subjects, true)
     
 end
