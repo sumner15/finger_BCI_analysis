3ee8d655e3cdfe7bb857c1bf79b9e3e8c662cc64
Sumner15 <28747498+sumner15@users.noreply.github.com>
2017-08-28 15:12:02 -0700

    Cleaning up superflous code before adding more functionality.

--- Finger_Functions/FINGER_Wadsworth/getERD.m
+++
@@ -1,8 +0,0 @@
-%Uses the result of 'datToMat', a data structure, to calculate the maximum
-%ERD on each trial and returns as a vector
-function ERD = getERD(dataIn)
-
-ERD = zeros(100,1);
-warning('getERD()is passing dummy data')
-
-end
