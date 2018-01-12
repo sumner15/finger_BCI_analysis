19a48f94bf9b55236867a09066843dac5cddaeb7
SumnerCaltech <SumnerCaltech@localhost>
2018-01-11 18:20:13 -0800

    tested stats across all 8 participants (rather than just 4)

--- FINGER_Wadsworth/acrossSubsBars.m
+++ FINGER_Wadsworth/acrossSubsBars.m
@@ -2,19 +2,21 @@
 % must run phase3_stats to populate workspace! 
 
 pVals = [1 1 1]; % index middle both (yellow vs. blue) 
+subsOfInterest = 1:8;
+nSubs = length(subsOfInterest);
 
 %% rearrange data
 latData = NaN(3,2,8);               % initialize to finger x cond x sub
 for i = 1:8
     latData(:,:,i) = latency{i};    % get data out of cell array -> array
 end
-latData = latData(:,:,[1 6 7 8]);   % subs of interest
+latData = latData(:,:,subsOfInterest);   % subs of interest
 
 
 %% get p values
 for finger = index:both
-    y = reshape(latData(finger,1,:),[1 4]);
-    b = reshape(latData(finger,2,:),[1 4]);
+    y = reshape(latData(finger,1,:),[1 nSubs]);
+    b = reshape(latData(finger,2,:),[1 nSubs]);
     
     % blocking (dLatency - each subject) tests b-y instead of b vs. y    
     [~,pVals(finger)] = ttest(b-y);
@@ -23,12 +25,10 @@ fprintf('p-values (Y vs. B), index: %1.3f, middle %1.3f, both: %1.3f\n',...
     pVals(1),pVals(2),pVals(3));
 
 %% get mean values for plotting
-barMeans = mean(latData,3);
-barStds = std(latData,0,3);        
+barMeans = nanmean(latData,3);
+barStds = nanstd(latData,0,3);        
 
 %% plot results
-% subsToPlot = 1:8;
-nSubsPlot = length(subsToPlot);
 set(figure,'Position',[100 20 700 500]);  
 set(0,'defaultAxesFontSize',20)
 
@@ -51,4 +51,4 @@ xticklabels({'index','middle','both'})
 xtickangle(45)      
 ylabel('latency (ms)')
 yticks([0 500 750 1000 1250 1500])    
-ylim([500 1500])
+ylim([600 1350])
