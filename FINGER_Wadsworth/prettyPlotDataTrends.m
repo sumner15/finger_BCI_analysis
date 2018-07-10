f0ac5d498b700f4bcb12e4be1bd689de9b73e4f4
SumnerCaltech <28747498+sumner15@users.noreply.github.com>
2018-07-10 15:35:27 -0700

    Updated data trends to use Spearman correlations & increased marker size

--- FINGER_Wadsworth/prettyPlotDataTrends.m
+++ FINGER_Wadsworth/prettyPlotDataTrends.m
@@ -15,23 +15,32 @@ set(figure,'Position',[100 20 600 500])
 set(0,'defaultlinelinewidth',2)
 % define subjects
 goodSubs = {'CHEA','RAZT','TRUL','VANT'};
-% create and plot fit line
+% create fit line (pearson correlation - assumes linear!)
 lm = fitlm(T.BBTscreenI(goodSubs),T.changeBBT(goodSubs),'linear');
-plot([min(T.BBTscreenI) max(T.BBTscreenI)],...
+% getting spearman correlation values (assumes ordinal monotonically increasing)
+[RHO,PVAL] = corr(T.BBTscreenI,T.changeBBT,'Type','Spearman');
+fprintf('Spearman Correlation (BBT screen | BBT change for all subjects):\t\t\trho = %1.3f\tp = %1.3f \n',RHO, PVAL)
+[RHO,PVAL] = corr(T.BBTscreenI(goodSubs),T.changeBBT(goodSubs),'Type','Spearman');
+fprintf('Spearman Correlation (BBT screen | BBT change for BCI-control subjects):\trho = %1.3f\tp = %1.3f \n',RHO, PVAL)
+
+% plotting
+h0 = plot([min(T.BBTscreenI) max(T.BBTscreenI)],...
     [lm.Coefficients{2,1}*(min(T.BBTscreenI))+lm.Coefficients{1,1} ...
      lm.Coefficients{2,1}*(max(T.BBTscreenI))+lm.Coefficients{1,1}],...
-     'color',[0 0.447 0.741],'Linewidth',2)
+     'color',[0 0.447 0.741],'Linewidth',2);
 hold on   
 % sort to plot in order
 T = sortrows(T,'RowNames');
 code = {'-ok','-ob','-or','-xr','-xb','-xk','-*k','-+k'};
 % plot each subject
 for sub = 1:size(T,1)
-    plot(T.BBTscreenI(sub),T.changeBBT(sub),code{sub})
+    plot(T.BBTscreenI(sub),T.changeBBT(sub),code{sub},'MarkerSize',15)
 end
 
 setType('BBT Change (blocks)', lm)
+grid minor
 % axis([0 30 0 10])
+set(h0,'visible','off')
     
     
 %% % plot BBT at baseline vs. finger movement latency
@@ -42,25 +51,35 @@ goodSubs = [1 6 7 8];
 labels = {'\delta latency index','\delta latency middle','\delta latency both','\delta latency'};
 
 set(figure,'Position',[100 20 600 500])            
-% create and plot fit line
+% create fit line (pearson correlation - assumes linear!)
 lm = fitlm(T.BBTscreenI(goodSubs),meanChangeLat(goodSubs),'linear');
-plot([min(T.BBTscreenI) max(T.BBTscreenI)],...
+% getting spearman correlation values (assumes ordinal monotonically increasing)
+[RHO,PVAL] = corr(T.BBTscreenI,meanChangeLat,'Type','Spearman');
+fprintf('Spearman Correlation (BBT screen | latency change for all subjects):\t\t\trho = %1.3f\tp = %1.3f \n',RHO, PVAL)
+[RHO,PVAL] = corr(T.BBTscreenI(goodSubs),meanChangeLat(goodSubs),'Type','Spearman');
+fprintf('Spearman Correlation (BBT screen | latency change for BCI-control subjects):\trho = %1.3f\tp = %1.3f \n',RHO, PVAL)
+
+ax = subplot(1,1,1);
+h1 = plot([min(T.BBTscreenI) max(T.BBTscreenI)],...
     [lm.Coefficients{2,1}*(min(T.BBTscreenI))+lm.Coefficients{1,1} ...
-     lm.Coefficients{2,1}*(max(T.BBTscreenI))+lm.Coefficients{1,1}],...
-     'color',[0 0.447 0.741],'Linewidth',2)
+     lm.Coefficients{2,1}*(max(T.BBTscreenI))+lm.Coefficients{1,1}],...     
+     'color',[0 0.447 0.741],'Linewidth',2);
 hold on   
 % plot     
 for sub = 1:size(T,1)
-    plot(T.BBTscreenI(sub),meanChangeLat(sub),code{sub})
+    plot(T.BBTscreenI(sub),meanChangeLat(sub),code{sub},'MarkerSize',15)
 end
 
 setType('latency change (s)', lm)
+ax.XMinorGrid = 'on'; %turns on the minor grid.
+ax.YGrid = 'on';
+set(h1,'visible','off')
 
 %% function to set type
 function setType(yAxisLabel, lm)
     xlabel('BBT Baseline (blocks)')
     ylabel(yAxisLabel)
-    grid on    
+%     grid on
     set(findall(gcf,'-property','FontSize'),'FontSize',20)
     set(findall(gcf,'-property','FontName'),'FontName','Arial')
     
@@ -69,4 +88,5 @@ function setType(yAxisLabel, lm)
         ',  p = ' num2str(round(lm.Coefficients.pValue(2),3))],...        
         'location','best');
     set(l1,'FontSize',15)    
+    legend off
 end
